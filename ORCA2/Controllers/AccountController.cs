81b033a868c611718dd3832ae3cc91c686cdee44
davis854 <davis854@marshall.edu>
2016-11-14 13:57:37 -0500

    Add space for testing

--- ORCA2/Controllers/AccountController.cs
+++ ORCA2/Controllers/AccountController.cs
@@ -20,6 +20,7 @@ namespace ORCA2.Controllers
 
         public AccountController()
         {
+            //
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
